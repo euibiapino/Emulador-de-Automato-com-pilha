85fb778b86c2a8d0de4f5855ca69dea0e1fa7ada
euibiapino <ibiapinojoaovitor@gmail.com>
2023-05-12 18:19:52 -0300

    Autômato com Pilha

---
+++ automatopilha.py
@@ -0,0 +1,62 @@
+def executa_automato(estados, estado_inicial, estadosFinais, alfabeto, alfabeto_pilha, regras, cadeia):
+
+    pilha = ["λ"]
+
+    estadoAtual = estado_inicial
+    posicaoCadeia = 0
+
+    while posicaoCadeia < len(cadeia):
+        simboloLido = cadeia[posicaoCadeia]
+        simboloTopoPilha = pilha[-1]
+
+        if simboloLido not in alfabeto or simboloTopoPilha not in alfabeto_pilha:
+            return False
+
+        if estadoAtual not in regras or simboloLido not in regras[estadoAtual] or simboloTopoPilha not in regras[estadoAtual][simboloLido]:
+            return False
+
+        novoEstado = regras[estadoAtual][simboloLido][simboloTopoPilha][1]
+        novoSimboloTopoPilha = regras[estadoAtual][simboloLido][simboloTopoPilha][0] if len(regras[estadoAtual][simboloLido][simboloTopoPilha]) > 1 else "λ"
+
+        if simboloTopoPilha != "λ":
+            pilha.pop()
+        if novoSimboloTopoPilha != "λ":
+            pilha.extend(novoSimboloTopoPilha[::-1])
+        estadoAtual = novoEstado
+
+        posicaoCadeia += 1
+
+    if posicaoCadeia == len(cadeia) and len(pilha) == 1 and pilha[0] == "λ" and estadoAtual in estadosFinais:
+        return True
+    else:
+        return False
+    
+nomeArquivo = 'c:/coloque aqui o caminho completo para o arquivo/entrada.txt'
+with open(nomeArquivo, 'r', encoding='utf-8') as arquivo:
+
+    conteudo = [linha.strip() for linha in arquivo.readlines()]
+
+    estados = conteudo[0].split()
+    estado_inicial = conteudo[1]
+    estadosFinais = conteudo[2].split()
+    alfabeto = conteudo[3].split()
+    alfabeto_pilha = conteudo[4].split()
+    regras = {}
+    for linha in conteudo[5:]:
+        elementos = linha.split()
+        if len(elementos) != 5:
+            continue
+        
+        estadoAtual, substituicao, simboloTopoPilhaoriginal, substituicaoresultante, estado_destino = elementos
+        if estadoAtual not in regras:
+            regras[estadoAtual] = {}
+        if substituicao not in regras[estadoAtual]:
+            regras[estadoAtual][substituicao] = {}
+        regras[estadoAtual][substituicao][simboloTopoPilhaoriginal] = (substituicaoresultante, estado_destino)
+
+cadeias_teste = conteudo[-2:]
+for cadeia in cadeias_teste:
+    if executa_automato(estados, estado_inicial, estadosFinais, alfabeto, alfabeto_pilha, regras, cadeia):
+        print("Aceita a cadeia:", cadeia)
+    else:
+        print("Rejeita a cadeia:", cadeia)
